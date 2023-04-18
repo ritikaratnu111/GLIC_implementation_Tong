5c3fe624637799e5c74341012e56fc9f047d994f
ritikaratnu111 <ritikaratnu111@iisc.ac.in>
2023-04-18 07:24:14 +0200

    thesis

---
+++ thesis.mm
@@ -0,0 +1,128 @@
+<map version="freeplane 1.7.0">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="Ideas" FOLDED="false" ID="ID_1622512464" CREATED="1676052086027" MODIFIED="1679982752383" STYLE="oval">
+<font SIZE="10"/>
+<hook NAME="MapStyle" background="#333333">
+    <properties edgeColorConfiguration="#808080ff,#00ddddff,#dddd00ff,#dd0000ff,#00dd00ff,#dd0000ff,#7cddddff,#dddd7cff,#dd7cddff,#7cdd7cff,#dd7c7cff,#7c7cddff" associatedTemplateLocation="template:/Darcula-1.7.mm" fit_to_viewport="false"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24.0 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" ICON_SIZE="12.0 pt" COLOR="#cccccc" STYLE="fork" ID="ID_755868815">
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#cccccc" BACKGROUND_COLOR="#3c3f41" TEXT_ALIGN="LEFT"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.selection" BACKGROUND_COLOR="#660000" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#660000"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#dddddd" STYLE="oval" SHAPE_HORIZONTAL_MARGIN="10.0 pt" SHAPE_VERTICAL_MARGIN="10.0 pt">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#ff3300">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#ffb439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#99ffff">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#bbbbbb">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,11"/>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="8" RULE="ON_BRANCH_CREATION"/>
+<node TEXT="Introduction" POSITION="right" ID="ID_1517892190" CREATED="1676052101962" MODIFIED="1679982752384">
+<edge COLOR="#00dddd"/>
+<font SIZE="10"/>
+<node TEXT="What is the problem" ID="ID_839650608" CREATED="1678682624401" MODIFIED="1679754985316">
+<font SIZE="10"/>
+</node>
+<node TEXT="Why is it important" ID="ID_1461533275" CREATED="1678682632192" MODIFIED="1679754985317">
+<font SIZE="10"/>
+</node>
+<node TEXT="Why has it not been solved before?" ID="ID_1768184168" CREATED="1678682644589" MODIFIED="1679754985318">
+<font SIZE="10"/>
+</node>
+<node TEXT="Aim of thesis" ID="ID_1658865997" CREATED="1679317183665" MODIFIED="1681795115304">
+<font SIZE="10"/>
+</node>
+<node TEXT="Key contributions" ID="ID_1295666494" CREATED="1679319674760" MODIFIED="1679754985320">
+<font SIZE="10"/>
+</node>
+</node>
+<node TEXT="Related work" POSITION="right" ID="ID_1783807038" CREATED="1676052122106" MODIFIED="1679982752384">
+<edge COLOR="#dddd00"/>
+<font SIZE="10"/>
+<node TEXT="What is the current state of art?" ID="ID_243565436" CREATED="1676052754957" MODIFIED="1679982752384">
+<font SIZE="10"/>
+</node>
+<node TEXT="Gap in the current state-of-art" ID="ID_1828645861" CREATED="1678682999268" MODIFIED="1679982752386">
+<font SIZE="10"/>
+</node>
+<node TEXT="How do you advance the state o art" ID="ID_392302377" CREATED="1678682921946" MODIFIED="1679982752388">
+<font SIZE="10"/>
+</node>
+</node>
+<node TEXT="Method" POSITION="right" ID="ID_1759563953" CREATED="1676052162347" MODIFIED="1679982752388">
+<edge COLOR="#dd0000"/>
+<font SIZE="10"/>
+<node TEXT="Conceptual description of method" ID="ID_993631417" CREATED="1678683079104" MODIFIED="1679310527730">
+<font SIZE="10"/>
+</node>
+<node TEXT="Mathematical Description of method" ID="ID_1480020193" CREATED="1678683092903" MODIFIED="1679310527731">
+<font SIZE="10"/>
+</node>
+</node>
+<node TEXT="Experiments" POSITION="right" ID="ID_1063228792" CREATED="1676052390523" MODIFIED="1679982752389">
+<edge COLOR="#00dd00"/>
+<font SIZE="10"/>
+<node TEXT="Experiments to verify the method" ID="ID_1326461526" CREATED="1678683780091" MODIFIED="1681795187792">
+<font SIZE="10"/>
+</node>
+<node TEXT="Results" ID="ID_681872878" CREATED="1678683801331" MODIFIED="1681795192225">
+<font SIZE="10"/>
+</node>
+</node>
+<node TEXT="Conclusion" POSITION="right" ID="ID_1136053638" CREATED="1678686391068" MODIFIED="1679982752389">
+<edge COLOR="#dddd7c"/>
+<font SIZE="10"/>
+<node TEXT="What does your model enable to do" ID="ID_1674799556" CREATED="1678686396123" MODIFIED="1681795196250">
+<font SIZE="10"/>
+</node>
+</node>
+</node>
+</map>
